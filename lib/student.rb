9634092f79d7dc89df0807c8e46e3ac00a7af869
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-27 05:46:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,16 @@
 require_relative "./user.rb"
 
 class Student  < User
+  def initialize
+    @knowledge = []
+  end
 
+  def learn(knowledge)
+    self.knowledge << knowledge
+  end
+
+  def knowledge
+    @knowledge
+  end
+    
 end
